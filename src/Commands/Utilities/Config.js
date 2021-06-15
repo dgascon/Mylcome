512b236db74abfbcfec2899874bd33bbbafe8354
dgascon <dgascon@student.42lyon.fr>
2021-06-16 01:14:38 +0200

    add command who show the config for the guild
    Command who show the config for the guild

---
+++ src/Commands/Utilities/Config.js
@@ -0,0 +1,34 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['conf'],
+			description: 'This provides the current configuration of bot for this guild',
+			category: 'Utilities'
+		});
+	}
+
+	async run(message) {
+		let data = this.client.utils.getDataByGuild(message);
+
+		const embed = new MessageEmbed()
+		embed.setColor('#0099ff')
+		embed.setTitle('Configuration')
+		embed.setTimestamp()
+		if (data)
+		{
+			for (var item in data)
+			{
+				embed.addField(item, data[item], false);
+			}
+		}
+		else
+		{
+			embed.setDescription("**No config for the guild**");
+		}
+		message.reply(embed).then(r => r.delete({timeout: this.client.delete_time}));
+	}
+}
