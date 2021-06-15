14676cccb127f4c52391d00b5d60bd54f47b66d4
dgascon <dgascon@student.42lyon.fr>
2021-06-16 01:15:41 +0200

    add command who set the name of canal for the guild

---
+++ src/Commands/Configurations/Setname.js
@@ -0,0 +1,24 @@
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['sname'],
+			description: 'This definie the name of canal',
+			category: 'Configurations'
+		});
+	}
+
+	async run(message, args) {
+		let text = args.join(' ');
+
+		if (text.length < 1 || text.length > 100)
+		{
+			message.reply(`The name of canal must contains between 1 and 100 character !`).then(r => r.delete({timeout: this.client.delete_time}));
+			return;
+		}
+		this.client.utils.updateDataByGuild(message, "name", args.join(' '));
+		message.reply(`The name of canal is now \`${args.join(' ')}\``).then(r => r.delete({timeout: this.client.delete_time}));
+	}
+}
