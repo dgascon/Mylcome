55576706c7c4c0610d50cecb9351214a82fef6b0
dgascon <dgascon@student.42lyon.fr>
2021-06-17 19:25:41 +0200

    add command for define the category
    New command for setting the category

---
+++ src/Commands/Configurations/Setcategory.js
@@ -0,0 +1,24 @@
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['scategory'],
+			description: 'This define the category of canal',
+			usage: 'scategory [name_of_category]',
+			category: 'Configurations'
+		});
+	}
+
+	async run(message, args) {
+		let category = message.guild.channels.cache.find(x => x.id === args[0] && x.type === "category");
+		if (!category) {
+			message.reply(`The category doesn't exist !`).then(r => r.delete({timeout: this.client.delete_time}));
+			return;
+		}
+
+		this.client.utils.updateDataByGuild(message, "category", args[0]);
+		message.reply(`The category is now <#${args[0]}>`).then(r => r.delete({timeout: this.client.delete_time}));
+	}
+}
