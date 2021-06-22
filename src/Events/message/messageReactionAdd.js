86e6071c0be2b74f456a8ff1b77c8d13ec24f972
dgascon <dgascon@student.42lyon.fr>
2021-06-22 12:24:30 +0200

    fix close channel when user has left the server

--- src/Events/message/messageReactionAdd.js
+++ src/Events/message/messageReactionAdd.js
@@ -29,23 +29,30 @@ module.exports = class extends Event {
 				const uidextract = message.content.match(RegExp(`<@[0-9]+>`))[0];
 				const uid = uidextract.substr(2, uidextract.length - 3);
 				const user = message.guild.members.cache.get(uid);
+
 				for (var key in react)
 				{
 					if (emoji === react[key][0]) {
 						for (let i = 1; i < react[key].length; i++) {
-							if (react[key][i] === "close") {
-								let rroles = this.client.jsonUtils.getKeyByGuild(message.guild.id, "removeroles");
-								if (rroles)
-								{
-									for (let i = 0; i < rroles.length; i++) {
-										await user.roles.remove(rroles[i]);
+							if (react[key][i] === "close")
+							{
+								if (user !== undefined) {
+									let rroles = this.client.jsonUtils.getKeyByGuild(message.guild.id, "removeroles");
+									if (rroles) {
+										for (let i = 0; i < rroles.length; i++) {
+											await user.roles.remove(rroles[i]);
+										}
 									}
 								}
 								await this.client.channels.cache.get(message.channel.id).delete("Finished");
 								return;
 							}
-							else
-								await user.roles.add(react[key][i]);
+							else {
+								if (user !== undefined)
+									await user.roles.add(react[key][i]);
+								else
+									message.channel.send('The user has left the server !').then(r => r.delete({timeout: this.client.delete_time}));
+							}
 						}
 					}
 				}
