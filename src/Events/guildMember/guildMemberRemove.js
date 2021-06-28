2371f97bc0100c60e1c6edcbf4098afe3c418bb0
dgascon <dgascon@student.42lyon.fr>
2021-06-28 04:21:45 +0200

    add save when a user has left to server

--- src/Events/guildMember/guildMemberRemove.js
+++ src/Events/guildMember/guildMemberRemove.js
@@ -1,4 +1,5 @@
 const Event = require('../../Structures/Event.js');
+const hastebin = require("hastebin-gen");
 
 module.exports = class extends Event {
 
@@ -15,7 +16,31 @@ module.exports = class extends Event {
 				let uid = message.content.match(/<@[!0-9]+>/).toString().replace(/[<@!>]/g, '');
 				if (member.id === uid)
 				{
-					channel.delete();
+					let save = this.client.jsonUtils.getKeyByGuild(message.guild.id, "save");
+					if (save)
+						save = member.guild.channels.cache.get(save);
+					if (save !== undefined)
+					{
+						await channel.messages.fetch({limit: 100})
+							.then(msg => {
+								let log = "";
+								msg = msg.array().reverse();
+								for (let j = 0; j < msg.length; j++) {
+									log += `${msg[j].author.tag} ${msg[j].createdAt.toString()}\n--\n`;
+									log += `${msg[j].content}\n`
+									log += `---------------------------------------------------------------------------------------------------------------\n`
+								}
+
+								hastebin(log, {extension: "txt"}).then(haste => {
+									save.send(`Logs of ${channel.name} => ${haste}`).then(r => {
+										channel.delete();
+									})
+								}).catch(error => {
+									console.error(error);
+								});
+							})
+					} else
+						channel.delete();
 				}
 			}
 		})
